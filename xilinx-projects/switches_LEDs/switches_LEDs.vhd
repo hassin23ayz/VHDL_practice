8be961b897353cb125c86530e40890d1359a414e
ayz03 <hassin23ayz@gmail.com>
2022-05-28 21:15:54 +0600

    a xilinix 14.7 ise project created

---
+++ xilinx-projects/switches_LEDs/switches_LEDs.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity switches_LEDs is
+    Port ( switch_0 : in  STD_LOGIC;
+           switch_1 : in  STD_LOGIC;
+           LED_0 : out  STD_LOGIC;
+           LED_1 : out  STD_LOGIC);
+end switches_LEDs;
+
+architecture Behavioral of switches_LEDs is
+
+begin
+
+
+end Behavioral;
+
