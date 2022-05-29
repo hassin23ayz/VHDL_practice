7f3c5679cb11067bd0fdfc6183f6f73f919caef3
ayz03 <hassin23ayz@gmail.com>
2022-05-29 18:29:19 +0600

    a lot file of xilinx is put into gitignore

---
+++ xilinx-projects/switches_LEDs/pa.fromNcd.tcl
@@ -0,0 +1,15 @@
+
+# PlanAhead Launch Script for Post PAR Floorplanning, created by Project Navigator
+
+create_project -name switches_LEDs -dir "/home/ayex/firmware_proj/VHDL_practice/xilinx-projects/switches_LEDs/planAhead_run_3" -part xc3s250evq100-5
+set srcset [get_property srcset [current_run -impl]]
+set_property design_mode GateLvl $srcset
+set_property edif_top_file "/home/ayex/firmware_proj/VHDL_practice/xilinx-projects/switches_LEDs/switches_LEDs.ngc" [ get_property srcset [ current_run ] ]
+add_files -norecurse { {/home/ayex/firmware_proj/VHDL_practice/xilinx-projects/switches_LEDs} }
+set_property target_constrs_file "constraints.ucf" [current_fileset -constrset]
+add_files [list {constraints.ucf}] -fileset [get_property constrset [current_run]]
+link_design
+read_xdl -file "/home/ayex/firmware_proj/VHDL_practice/xilinx-projects/switches_LEDs/switches_LEDs.ncd"
+if {[catch {read_twx -name results_1 -file "/home/ayex/firmware_proj/VHDL_practice/xilinx-projects/switches_LEDs/switches_LEDs.twx"} eInfo]} {
+   puts "WARNING: there was a problem importing \"/home/ayex/firmware_proj/VHDL_practice/xilinx-projects/switches_LEDs/switches_LEDs.twx\": $eInfo"
+}
--- xilinx-projects/switches_LEDs/switches_LEDs.vhd
+++ xilinx-projects/switches_LEDs/switches_LEDs.vhd
@@ -2,8 +2,9 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity switches_LEDs is
-    Port ( switch_0 : in  STD_LOGIC;
-           switch_1 : in  STD_LOGIC;
+    Port ( 
+	        --switch_0 : in  STD_LOGIC;
+           --switch_1 : in  STD_LOGIC;
            LED_0 : out  STD_LOGIC;
            LED_1 : out  STD_LOGIC);
 end switches_LEDs;
@@ -11,7 +12,10 @@ end switches_LEDs;
 architecture Behavioral of switches_LEDs is
 
 begin
-
-
+	--LED_0 <= switch_0;
+	--LED_1 <= switch_1;
+	LED_0 <= '0';
+	LED_1 <= '1';
+	
 end Behavioral;
 
